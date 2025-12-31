3a52dc15f2ae19b8f4356be4c3ed0718406e3ee9
tariq salama <tariq_mu7amad@yahoo.com>
2025-12-31 10:40:15 +0200

    update vito

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/EPROM/", // CRITICAL: Must match your GitHub repository name
 })
