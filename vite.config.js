5079021d8cd9a32e0645213cc01a32ed7ae489fa
tariq salama <tariq_mu7amad@yahoo.com>
2025-12-31 14:44:19 +0200

    clear

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/EPROM/", // <--- CHANGE THIS to match your GitHub Repository name exactly (e.g., /EPROM/ or /IdeaBank/)
-})
